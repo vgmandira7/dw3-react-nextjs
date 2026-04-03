36890d0bd7015f38231c49bd9418f932d6cfc3b0
DSM3 <fatec-dsm3@academico.intra>
2026-04-02 22:50:59 -0300

    Primeiro commit em react

---
+++ src/components/MeuComponente.js
@@ -0,0 +1,13 @@
+//componentes funcionais
+const MeuComponente = () => {
+    return (
+        <>
+        <h1>Teste</h1>
+        <h2>Outro teste</h2>
+        </>
+    )
+    
+
+}
+
+export default MeuComponente;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import MeuComponente from "@/components/MeuComponente";
+
+export default function Home() {
+  return (
+    <>
+      {/* Seu conteúdo aqui */}
+      <h1>Home</h1>
+      <MeuComponente />
+      
+    </>
+  );
+}
